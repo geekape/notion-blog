54f3340d4fe69aa906f9a54f8df42d2d715cabd6
yuanmeng <861013016@qq.com>
2020-10-29 16:56:19 +0800

    chore: init notion blog

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/global.css'
+import 'katex/dist/katex.css'
+
+export default ({ Component, pageProps }) => (
+  <>
+    <Component {...pageProps} />
+  </>
+)
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Header from '../components/header'
+import ExtLink from '../components/ext-link'
+import Features from '../components/features'
+import GitHub from '../components/svgs/github'
+import sharedStyles from '../styles/shared.module.css'
+
+export default () => (
+  <>
+    <Header titlePre="Home" />
+    <div className={sharedStyles.layout}>
+      <h1>My Name Is YuanMeng</h1>
+    </div>
+  </>
+)
