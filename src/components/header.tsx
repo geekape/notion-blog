462ec41bf16136b14d7488d41693b92880776ce4
yuanmeng <861013016@qq.com>
2020-11-01 11:41:20 +0800

    feat: 修改网站title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,12 +18,12 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} 钟储兵博客</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
         />
-        <meta name="og:title" content="My Notion Blog" />
+        <meta name="og:title" content="钟储兵博客" />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@_ijjk" />
         <meta name="twitter:card" content="summary_large_image" />
